8277133cb2a73da6a79ae6d2eef47d2d18583f68
Abdulazeez <hazeezadediran@gmail.com>
2025-02-19 20:14:26 +0800

    Wrap Coming Soon component in a fragment and comment out Analytics import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import ComingSoon from './pages/ComingSoon';
+// import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
+    <>
       <ComingSoon />
+      {/* <Analytics id="ANALYTICS_ID" /> */}
+    </>
   );
 }
 
